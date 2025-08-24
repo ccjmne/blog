7ae702b1136790f9df4cd6e1b0ba4492cdf7c324
Éric NICOLAS <ccjmne@gmail.com>
2025-08-25 00:07:16 +0200

    Define terminal-to-html's term-fg colours in theme

--- scripts/to-html.sh
+++ scripts/to-html.sh
@@ -1,5 +1,5 @@
 {
     echo -n '<pre class="z-code"><code>'
-    terminal-to-html | sed -e 's/ term-fg1//g; s/term-fg31/z-string/g; s/term-fg32/z-variable/g; s/term-fg33/z-constant/g'
+    terminal-to-html
     echo -n '</pre></code>'
 } | wl-copy
