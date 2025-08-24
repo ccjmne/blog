f49c2c6ce7b9b59a671230670fe951027a4be1af
Éric NICOLAS <ccjmne@gmail.com>
2025-08-24 18:53:59 +0200

    Switch dark theme from Mocha to Macchiato

--- scripts/update-catppuccin.sh
+++ scripts/update-catppuccin.sh
@@ -2,12 +2,12 @@
 
 readonly project="$(readlink -f "$0" | xargs dirname | xargs dirname)"
 
-cargo run --manifest-path update-catppuccin/Cargo.toml -- \
+cargo run --manifest-path "$project/scripts/update-catppuccin/Cargo.toml" -- \
     <(curl -Ls https://github.com/catppuccin/sublime-text/raw/refs/heads/main/build/Catppuccin%20Latte.sublime-color-scheme) \
     > "$project/static/latte.css"
 
-cargo run --manifest-path update-catppuccin/Cargo.toml -- \
-    <(curl -Ls https://github.com/catppuccin/sublime-text/raw/refs/heads/main/build/Catppuccin%20Mocha.sublime-color-scheme) \
+cargo run --manifest-path "$project/scripts/update-catppuccin/Cargo.toml" -- \
+    <(curl -Ls https://github.com/catppuccin/sublime-text/raw/refs/heads/main/build/Catppuccin%20Macchiato.sublime-color-scheme) \
     > "$project/static/mocha.css"
 
 t=$(mktemp --directory)
