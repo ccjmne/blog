0b3d6fd4abdd45275f454330fd9018ca76cd06c4
Éric NICOLAS <ccjmne@gmail.com>
2025-08-24 19:45:16 +0200

    Get full Outfit font at weight 500

--- scripts/prepare-fonts.sh
+++ scripts/prepare-fonts.sh
@@ -2,13 +2,11 @@
 
 readonly project="$(readlink -f "$0" | xargs dirname | xargs dirname)"
 
-# Get only the following glyphs: 0123456789+-,.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-readonly outfit=$(curl -L 'https://fonts.googleapis.com/css2?family=Outfit:wght@400&text=0123456789%2B-%2C.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' | grep -Po '(?<=url\()[^)]+')
-
 # Get only the anchor: ⚓
 readonly noto=$(curl -L 'https://fonts.googleapis.com/css2?family=Noto+Sans+Symbols&text=%e2%9a%93' | grep -Po '(?<=url\()[^)]+')
 
 # Get the full font
+readonly outfit=$(curl -L 'https://fonts.googleapis.com/css2?family=Outfit:wght@500' | grep -Po '(?<=url\()[^)]+')
 readonly hedvig=$(curl -L 'https://fonts.googleapis.com/css2?family=Hedvig+Letters+Serif' | grep -Po '(?<=url\()[^)]+')
 readonly fira=$(curl -L 'https://fonts.googleapis.com/css2?family=Fira+Code' | grep -Po '(?<=url\()[^)]+')
 
