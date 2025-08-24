0ada21a9d1824a38a9dc3d4f00dbea7279f23d2a
Éric NICOLAS <ccjmne@gmail.com>
2025-08-24 19:17:40 +0200

    Get only the Anchor from Noto

--- scripts/prepare-fonts.sh
+++ scripts/prepare-fonts.sh
@@ -4,8 +4,12 @@ readonly project="$(readlink -f "$0" | xargs dirname | xargs dirname)"
 
 # Get only the following glyphs: 0123456789+-,.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 readonly outfit=$(curl -L 'https://fonts.googleapis.com/css2?family=Outfit:wght@400&text=0123456789%2B-%2C.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' | grep -Po '(?<=url\()[^)]+')
+
+# Get only the anchor: ⚓
+readonly noto=$(curl -L 'https://fonts.googleapis.com/css2?family=Noto+Sans+Symbols&text=%e2%9a%93' | grep -Po '(?<=url\()[^)]+')
+
+# Get the full font
 readonly hedvig=$(curl -L 'https://fonts.googleapis.com/css2?family=Hedvig+Letters+Serif' | grep -Po '(?<=url\()[^)]+')
-readonly noto=$(curl -L 'https://fonts.googleapis.com/css2?family=Noto+Sans+Symbols' | grep -Po '(?<=url\()[^)]+')
 readonly fira=$(curl -L 'https://fonts.googleapis.com/css2?family=Fira+Code' | grep -Po '(?<=url\()[^)]+')
 
 for font in outfit hedvig noto fira; do
