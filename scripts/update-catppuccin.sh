81f516e2141220609851cbca3c81b32fa8efb3b8
Éric NICOLAS <ccjmne@gmail.com>
2026-01-16 00:37:59 +0100

    Drop compile-catppuccin-syntax and its usage
    Zola now ships since 0.22.0 with Catppuccin syntax highlighting
    configurations.

--- scripts/update-catppuccin.sh
+++ scripts/update-catppuccin.sh
@@ -2,14 +2,6 @@
 
 readonly project="$(readlink -f "$0" | xargs dirname | xargs dirname)"
 
-cargo run --manifest-path "$project/scripts/compile-catppuccin-syntax/Cargo.toml" -- \
-    <(curl -Ls https://github.com/catppuccin/sublime-text/raw/refs/heads/main/build/Catppuccin%20Latte.sublime-color-scheme) \
-    > "$project/static/light.css"
-
-cargo run --manifest-path "$project/scripts/compile-catppuccin-syntax/Cargo.toml" -- \
-    <(curl -Ls https://github.com/catppuccin/sublime-text/raw/refs/heads/main/build/Catppuccin%20Frappe.sublime-color-scheme) \
-    > "$project/static/dark.css"
-
 t=$(mktemp --directory)
 ( cd $t && pnpm install @catppuccin/palette )
 mv $t/node_modules/@catppuccin/palette/scss/_catppuccin.scss "$project/sass"
