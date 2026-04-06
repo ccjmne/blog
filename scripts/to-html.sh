64ff56cc0784ab88c2613a1319293385477dbd13
Éric NICOLAS <ccjmne@gmail.com>
2026-04-06 23:11:59 +0200

    Introduce tohtml-adjust for Neovim's TOhtml output
    Will attempt to map some tree-sitter-derived classes to some more
    semantically ANSI-grounded semantics that I've set up in thme.scss.

--- scripts/to-html.sh
+++ scripts/to-html.sh
@@ -1,5 +1,5 @@
 {
-    echo -n '<pre class="giallo z-code"><code data-lang="shellscript">'
+    echo -n '<pre class="giallo z-code"><code data-lang="plain">'
     terminal-to-html
     echo -n '</code></pre>'
 } | wl-copy
