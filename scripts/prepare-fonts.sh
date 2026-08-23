679d21cfb26de757da65a5a11606a0de63d996e2
Éric NICOLAS <ccjmne@gmail.com>
2026-08-24 00:01:27 +0200

    Only preload above-fold fonts, use 'display' hints

--- scripts/prepare-fonts.sh
+++ scripts/prepare-fonts.sh
@@ -3,9 +3,9 @@
 readonly project="$(readlink -f "$0" | xargs dirname | xargs dirname)"
 
 # Get the full fonts:
-readonly ouft=$(curl -Ls 'https://fonts.googleapis.com/css2?family=Outfit:wght@500')
-readonly work=$(curl -Ls 'https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,400;0,600;1,400')
-readonly fira=$(curl -Ls 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;600')
+readonly ouft=$(curl -Ls 'https://fonts.googleapis.com/css2?family=Outfit:wght@500&display=swap')
+readonly work=$(curl -Ls 'https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,400;0,600;1,400&display=swap')
+readonly fira=$(curl -Ls 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;600&display=swap')
 readonly noto=$(curl -Ls 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500&display=optional')
 
 preload=()
@@ -16,8 +16,10 @@ for font in ouft work fira noto; do
         file=$font-$((i++))
         curl -Ls $(grep -Po '(?<=url\()[^)]+' <<< "$ff") -o $file.ttf
         woff2_compress $file.ttf
-        preload+=("<link rel=\"preload\" href=\"{{ get_url(path=\"/$file.woff2\") }}\" as=\"font\" type=\"font/woff2\" crossorigin>")
-        style+=("  $(echo $ff | sed "s,url[^)]*),url('{{ get_url(path=\"/$file.woff2\") }}')," | sed "s,format[^)]*),format('woff2'),")")
+        if [[ $file == ouft-1 || $file == work-[23] ]]; then # above-the-fold fonts
+            preload+=("<link rel=\"preload\" href=\"{{ get_url(path=\"/$file.woff2\", cachebust=true) }}\" as=\"font\" type=\"font/woff2\" crossorigin>")
+        fi
+        style+=("  $(echo $ff | sed "s|url[^)]*)|url('{{ get_url(path=\"/$file.woff2\", cachebust=true) }}')|" | sed "s,format[^)]*),format('woff2'),")")
     done < <(echo "${!font}" | sed -n 'H; /^}$/ { x;s/\n\s*/ /g;p;s/.*//;h }')
 done
 
